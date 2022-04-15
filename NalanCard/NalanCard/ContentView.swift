639642acd67dde09f36f09bb51c9c489ef4e1b82
Nalan Duman <dumann16@itu.edu.tr>
2022-04-16 00:24:38 +0300

    Created Project

---
+++ NalanCard/NalanCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NalanCard
+//
+//  Created by Nalan Duman on 16.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NalanCard/NalanCard/NalanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NalanCardApp.swift
+//  NalanCard
+//
+//  Created by Nalan Duman on 16.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NalanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
